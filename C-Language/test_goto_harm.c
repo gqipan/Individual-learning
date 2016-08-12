a9b396923287238f0ebe9e51b34199df55a6f221
PanPan <gongqipan@qipeipu.com>
2016-08-12 15:09:08 +0800

    修改注释

--- C-Language/test_goto_harm.c
+++ C-Language/test_goto_harm.c
@@ -1,22 +1,7 @@
 #include <stdio.h>
 #include <malloc.h>
 
-void func(int n)
-{
-    int* p = NULL;
-
-    if(  n < 0 )
-    {
-        goto STATUS; //No Free memory
-    }
-
-    p = (int*)malloc(sizeof(int) * n);
-    
-STATUS:
-    p[0] = n;    
-    
-    free(p);
-}
+void func(int n);
 
 int main()
 {  
@@ -28,9 +13,27 @@ int main()
     
     printf("func(-1)\n");
     
-    func(-1);
+    func(-1); //发生段错误
     
     printf("end...\n");
     
     return 0;
+}
+
+
+void func(int n)
+{
+    int* p = NULL;
+
+    if(  n < 0 )
+    {
+        goto STATUS; /
+    }
+
+    p = (int*)malloc(sizeof(int) * n);
+    
+STATUS:
+    p[0] = n;   //如果是goto过来的，没有申请内存就去使用内存并且释放内存 
+    
+    free(p);	//段错误
 }
