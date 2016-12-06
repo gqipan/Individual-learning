948acb53db0b79b9f8b534d11680874daa7c8736
panpan <gqp201210@gmail.com>
2016-12-06 23:37:32 +0800

    start

--- Java/EduMS/src/main/java/org/pan/ems/entities/City.java
+++ Java/EduMS/src/main/java/org/pan/ems/entities/City.java
@@ -1,36 +1,15 @@
 package org.pan.ems.entities;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class City {
-	// id
 	private Integer cityId;
-	// 城市名
+
 	private String cityName;
-
-	public Integer getCityId() {
-		return cityId;
-	}
-
-	public void setCityId(Integer cityId) {
-		this.cityId = cityId;
-	}
-
-	public String getCityName() {
-		return cityName;
-	}
-
-	public void setCityName(String cityName) {
-		this.cityName = cityName;
-	}
-
-	public City() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public City(Integer cityId, String cityName) {
-		super();
-		this.cityId = cityId;
-		this.cityName = cityName;
-	}
-
 }
