0e8ad29a2645d7b512ed188d4ea0f02bc8ba685d
QiPan <gqp201210@gmail.com>
2017-08-21 17:02:23 +0800

    修改

---
+++ concurrency-practice/src/main/java/com/pan/syncContainers/Tickets.java
@@ -0,0 +1,18 @@
+package com.pan.syncContainers;
+
+import java.util.Collections;
+import java.util.Vector;
+
+/**
+ *  多线程使用Vector或者HashTable的示例（简单线程同步问题）
+ */
+public class Tickets {
+
+    public static void main(String[] args) {
+        // 初始化火车票池并添加火车票:避免线程同步可采用Vector替代ArrayList  HashTable替代HashMap
+        final Vector<String> tickets = new Vector<>();
+
+
+
+    }
+}
