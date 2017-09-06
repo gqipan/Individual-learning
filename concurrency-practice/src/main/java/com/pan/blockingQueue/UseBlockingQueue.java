b78d17110f92839fd191c35522ba47a80c899eef
QiPan <gqp201210@gmail.com>
2017-09-06 10:12:19 +0800

    修改

---
+++ concurrency-practice/src/main/java/com/pan/blockingQueue/UseBlockingQueue.java
@@ -0,0 +1,20 @@
+package com.pan.blockingQueue;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * Author: Qipan.G
+ * Date: 2017/9/5
+ * Time: 14:22
+ * Descriptions:
+ */
+public class UseBlockingQueue {
+
+
+    public static void main(String[] args) {
+        BlockingQueue blockingQueue = new ArrayBlockingQueue(1024);
+
+    }
+
+}
