c27afd2f87774bbcd8f1d79c9d3b84932261f05d
Pan <gqp201210@gmail.com>
2017-09-14 21:37:44 +0800

    修改

--- concurrency-practice/src/main/java/com/pan/blockingQueue/UseBlockingQueue.java
+++ concurrency-practice/src/main/java/com/pan/blockingQueue/UseBlockingQueue.java
@@ -26,18 +26,18 @@ public class UseBlockingQueue {
 //
 //        TimeUnit.SECONDS.sleep(4);
 
-        BlockingQueue<String> queue = new ArrayBlockingQueue<>(2);
+        BlockingQueue<String> queue = new ArrayBlockingQueue<>(3);
         queue.put("a");
         queue.put("b");
 
         // ========== 添加元素 put 会阻塞，offer 只会返回成功失败，add 无空闲位置，抛出异常 =============
-//        queue.put("c"); // 超过了队列的界限也会阻塞，等待有空闲的位置
+        queue.put("c"); // 超过了队列的界限也会阻塞，等待有空闲的位置
 
 //        boolean offerResult = queue.offer("c");// 使用 offer，超过了队列的界限只会返回false，并不会阻塞
 //        System.out.println(offerResult);
 
         // 等待 TimeUnit 时间，还没有空闲的位置，那么返回false
-        System.out.println(queue.offer("c", 3, TimeUnit.SECONDS));
+//        System.out.println(queue.offer("c", 3, TimeUnit.SECONDS));
 
 //        boolean addResult = queue.add("c"); // 使用 add 超过了队列的限制会抛出异常，IllegalStateException：Queue full， 不会阻塞
 //        System.out.println(addResult);
@@ -47,14 +47,15 @@ public class UseBlockingQueue {
 //        System.out.println("poll: " + queue.poll());
 //        // 执行方法，如果队列中没有元素，等待3秒，3秒后还是没有元素，返回null
 //        System.out.println("poll: " + queue.poll(3, TimeUnit.SECONDS));
-//        System.out.println("remove: " + queue.remove("a")); // 返回True Or False, 并且移出特定的元素
+//        System.out.println("remove: " + queue.remove());
+        System.out.println("remove: " + queue.remove("b")); // 返回True Or False, 并且移出特定的元素
 //
 //
 //        // =======检查方法 peek、element 检查当前元素存不存在， 存在返回当前元素=============
-//        System.out.println("element: " + queue.element());  // element 返回当前索引的元素，指向即将 peek 的元素，游标不会动
+        System.out.println("element: " + queue.element());  // element 返回当前索引的元素，指向即将 peek 的元素，游标不会动
 //        System.out.println("element: " + queue.element());  // 如果元素为null  抛出异常
 //
-//        System.out.println("peek: " + queue.peek()); // peek 特使获取当前位置的元素，不会抛出异常
+        System.out.println("peek: " + queue.peek()); // peek 特使获取当前位置的元素，不会抛出异常
 //        System.out.println("peek: " + queue.peek()); // 不存在返回null
 //
 //
