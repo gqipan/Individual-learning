559974f5fc2d019283b1a00f70d9778cc2d38220
QiPan <gqp201210@gmail.com>
2017-09-20 10:32:06 +0800

    修改

--- concurrency-practice/src/main/java/com/pan/threadPool/ScheduledJob.java
+++ concurrency-practice/src/main/java/com/pan/threadPool/ScheduledJob.java
@@ -1,6 +1,9 @@
 package com.pan.threadPool;
 
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Author: Qipan.G
@@ -12,7 +15,11 @@ public class ScheduledJob {
 
     public static void main(String[] args) {
         Temp temp = new Temp();
-        Executors.newScheduledThreadPool(1);
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+//        Executors.newSingleThreadScheduledExecutor(T)
+        // 初始时延迟5秒后开始执行，执行周期为每秒一次
+        ScheduledFuture<?> scheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(temp, 5, 1, TimeUnit.SECONDS);
     }
 
 }
