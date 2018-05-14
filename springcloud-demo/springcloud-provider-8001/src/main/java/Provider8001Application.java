1e40bf6584033b3f88a22e711458d45d645a4dff
Qipan <gongqipan@qipeipu.com>
2018-05-14 19:01:01 +0800

    暂存

---
+++ springcloud-demo/springcloud-provider-8001/src/main/java/Provider8001Application.java
@@ -0,0 +1,17 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: Qipan.G
+ * Date: 2018/5/14
+ * Time: 16:56
+ * Descriptions: 主启动类
+ */
+@SpringBootApplication
+public class Provider8001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Provider8001Application.class, args);
+    }
+
+}
